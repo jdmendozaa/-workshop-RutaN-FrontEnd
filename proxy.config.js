8ada14fc6835ff2f1a6cfa78b6992cd710ef529b
canojuanpa800 <jcanor@unal.edu.co>
2022-05-31 10:43:52 -0500

    environment back-end host

--- proxy.config.js
+++ proxy.config.js
@@ -1,6 +1,6 @@
 const PROXI_CONFIG = {
   "/api/": {
-    "target" :"http://${BACKEND_HOST}:8080",
+    "target" :"http://"+process.env.BACKEND_HOST+":8080",
     "secure": false,
     "logLevel": "debug"
   }
